de9a5bdabf137208680bc68043d5ea5493e0dec7
lathropd <daniel.lathrop@gmail.com>
2014-09-11 01:32:04 -0500

    renamed actions-v0 to actions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $.getScript('http://res.dallasnews.com/resrsc/js/biomniture.js');
 $.getScript('../js/picturefill.js');
-$.getScript('../js/common-v1.js');
-$.getScript('../js/actions-v0.js');
+$.getScript('../js/common.js');
+$.getScript('../js/actions.js');
 
 
 
