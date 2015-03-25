8010d46bf82dc24d93af721393e268710b5b22fc
John Hancock <jdhancock@gmail.com>
2015-03-25 08:59:35 -0500

    Numerous fixes
    — Fixes to the header to add subscribe link, new url for guideline
    — Replaced some h1 tags in code blocks with h5 tags
    — Numerous fixes to CSS to streamline things
    — Removed call to bootstrap in index file. Also added bold italic for
    Open Sans to font list, removed picturefill code.
    — Added in “published on line”
    — Adjusted position of custom javascript call so that it calls before
    gigya scripts run
    — Added a custom.js file

--- common/js/responsive-header.js
+++ common/js/responsive-header.js
@@ -12,7 +12,7 @@ document.write("<ul class=\"navList\">");
 document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/news\/\">News<\/a><\/li>");
 document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/business\/\">Business<\/a><\/li>");
 document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/sports\/\">Sports<\/a><\/li>");
-document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/entertainment\/\">Entertainment<\/a><\/li>");
+document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.guidelive.com\">Entertainment<\/a><\/li>");
 document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/opinion\/\">Opinion<\/a><\/li>");
 document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/lifestyles\/\">Life<\/a><\/li>");
 document.write("<li><a class=\"sectionLink\" href=\"http:\/\/www.dallasnews.com\/photos\/\">Photos<\/a><\/li>");
@@ -20,11 +20,12 @@ document.write("<li><a class=\"sectionLink\" href=\"http:\/\/video.dallasnews.co
 document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/m.cars.com\/?aff=dallas\">Autos<\/a><\/li>");
 document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/findnsave.dallasnews.com\/stores\/\">Shopping<\/a><\/li>");
 document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/secure.adpay.com\/SearchMobile.aspx?p=1013&city=1013\/\">Classifieds<\/a><\/li>");
-document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/m.realestate.dallasnews.com\/eng\/sales\/tx-usa\">Real Estate<\/a><\/li>");
+document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/realestate.dallasnews.com\">Real Estate<\/a><\/li>");
 document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/m.local-jobs.monster.com\/jobsearch\/jobs?ch=dallasmorning&where=Dallas\">Jobs<\/a><\/li>");
 document.write("<li><a class=\"sectionLink marketplace\" href=\"http:\/\/m.legacy.com\/obituaries\/dallasmorningnews\">Obituaries<\/a><\/li>");
 document.write("<li><a class=\"sectionLink fico icon-newspaper\" href=\"https:\/\/myaccount.dallasnews.com\/signin-olive.html?from=http%3a%2f%2ftablet.olivesoftware.com%2fOlive%2fTablet%2fDallasMorningNews%2fAfterLogin.ashx\">ePaper<\/a><\/li>");
 document.write("<li><a class=\"sectionLink fico icon-user\" href=\"https:\/\/myaccount.dallasnews.com\/\">My Account<\/a><\/li>");
+document.write("<li><a class=\"sectionLink fico icon-pencil\" href=\"http:\/\/www.dallasnews.com\/subscribe\">Subscribe<\/a><\/li>");
 document.write("<\/ul>");
 document.write("<\/nav>");
 
---
+++ starterkit/js/customJS.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	
+	//custom scripting goes here
+
+});
